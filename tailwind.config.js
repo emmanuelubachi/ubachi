dd56ee7f782f392f6bd531e234b40c7ed846e842
emmanuelubachi <emmanuelubachi@gmai.com>
2023-09-05 23:49:30 +0400

    update tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  darkMode: ["class"],
+  // content: [
+  //   './pages/**/*.{ts,tsx}',
+  //   './components/**/*.{ts,tsx}',
+  //   './app/**/*.{ts,tsx}',
+  //   './src/**/*.{ts,tsx}',
+  // ],
+  content: [
+    "./node_modules/flowbite/**/*.js",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        xs: "475px",
+        ...defaultTheme.screens,
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+          950: "#172554",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate"), require("flowbite/plugin")],
+};

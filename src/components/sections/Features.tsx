f449963b59458a4f459d24547536247d35d8fe66
emmanuelubachi <emmanuelubachi@gmai.com>
2023-09-06 16:59:01 +0400

    update sections

--- src/components/sections/Features.tsx
+++ src/components/sections/Features.tsx
@@ -4,16 +4,17 @@ import FeatureCard from "../cards/FeatureCard";
 const Features = () => {
   return (
     <section>
+      <hr className="h-px max-w-screen-xl mx-auto bg-gray-200 border-0" />
       <div className="max-w-screen-xl m-auto px-4 py-8 lg:px-6 sm:py-16 lg:py-24">
         <div className="max-w-2xl mx-auto text-center mb-16 lg:mb-20">
-          <h2 className="text-3xl font-extrabold leading-tight tracking-tight text-primary-800 sm:text-4xl">
-            Our work
+          <h2 className="text-3xl font-extrabold leading-tight tracking-tight text-primary-900 sm:text-4xl">
+            What I Do
           </h2>
           <p
             className="mt-4 font-normal
             text-base
             sm:text-lg
-          text-gray-500  dark:text-gray-400"
+          text-gray-500"
           >
             Crafted with skill and care to help our clients grow their business!
           </p>
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -7,22 +7,26 @@ import ProjectB from "../projects/ProjectB";
 const Projects = () => {
   return (
     <section>
-      <div className="max-w-screen-xl m-auto px-4 py-8 lg:px-6 sm:py-16 lg:py-20">
+      <hr className="h-px max-w-screen-xl mx-auto bg-gray-200 border-0" />
+      <div className="max-w-screen-xl m-auto px-4 py-8 lg:px-6 sm:py-16 lg:py-24">
         <div className="max-w-2xl mx-auto text-center mb-16 lg:mb-20">
-          <h2 className="text-3xl font-extrabold leading-tight tracking-tight text-primary-800 sm:text-4xl">
-            Our work
+          <h2 className="text-3xl font-extrabold leading-tight tracking-tight text-primary-900 sm:text-4xl">
+            Projects
           </h2>
           <p
             className="mt-4 font-normal
             text-base
             sm:text-lg
-          text-gray-500  dark:text-gray-400"
+          text-gray-500"
           >
             Crafted with skill and care to help our clients grow their business!
           </p>
         </div>
-        <ProjectA />
-        <ProjectB />
+        <div className="space-y-12 sm:space-y-16">
+          <ProjectA />
+          <ProjectB />
+          <ProjectA />
+        </div>
       </div>
     </section>
   );

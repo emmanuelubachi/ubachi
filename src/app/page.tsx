fddcc261e87a18e1c8e5cbf5264746417ab7e57e
emmanuelubachi <emmanuelubachi@gmai.com>
2023-09-06 16:58:33 +0400

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Navbar from "@/components/body/Navbar";
+import Contact from "@/components/sections/Contact";
 import Features from "@/components/sections/Features";
 import Hero from "@/components/sections/Hero";
 import Projects from "@/components/sections/Projects";
+import Resume from "@/components/sections/Resume";
 
 export default function Home() {
   return (
     <>
-      <header className="mb-4">
+      <header>
         <Navbar />
         <Hero />
       </header>
       <main>
         <Features />
         <Projects />
+        <Resume />
+        <Contact />
       </main>
     </>
   );

fccf59e252b1b15c4fbbde68c06f2921e6d52bdf
emmanuelubachi <emmanuelubachi@gmai.com>
2023-09-13 23:14:35 +0400

    update body

--- src/components/body/Navbar.tsx
+++ src/components/body/Navbar.tsx
@@ -2,13 +2,43 @@
 import React from "react";
 import { useEffect } from "react";
 import { initFlowbite } from "flowbite";
+import Link from "next/link";
 
 const Navbar = () => {
   useEffect(() => {
     initFlowbite();
   }, []);
+
+  const navList = [
+    {
+      id: 1,
+      name: "Home",
+      url: "#",
+    },
+    {
+      id: 2,
+      name: "What I Do",
+      url: "#features",
+    },
+    {
+      id: 3,
+      name: "Projects",
+      url: "#projects",
+    },
+    {
+      id: 4,
+      name: "Resume",
+      url: "#resume",
+    },
+    {
+      id: 5,
+      name: "Contact",
+      url: "#contact",
+    },
+  ];
+
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="fixed w-full z-20 top-0 left-0 bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://ubachi.com/" className="flex items-center">
           <span className="text-gray-600 self-center text-2xl lg:text-3xl font-semibold whitespace-nowrap">
@@ -31,18 +61,25 @@ const Navbar = () => {
             type="button"
             className="text-white 
             bg-primary-900 
-            rounded-lg px-5 py-3 mr-3 md:mr-0
-            font-medium text-sm text-center
+            rounded-lg 
+            px-3 py-1 sm:px-5 sm:py-3
+            mr-3 md:mr-0
+            font-medium 
+            text-xs sm:text-sm text-center
             hover:bg-gradient-to-tr hover:from-primary-800 hover:to-pink-700
             transition-transform hover:scale-110 cursor-pointer
             focus:ring-4 focus:outline-none focus:ring-blue-300"
           >
             BOOK A CALL
           </button>
-          {/* <button
+          <button
             data-collapse-toggle="navbar-cta"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2
+            sm:w-10 sm:h-10
+            justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 
+            focus:outline-none focus:ring-2 focus:ring-gray-200 
+            dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-cta"
             aria-expanded="false"
           >
@@ -62,48 +99,29 @@ const Navbar = () => {
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
-          </button> */}
+          </button>
         </div>
-        {/* <div
+        <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-cta"
         >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Contact
-              </a>
-            </li>
+            {navList.map((items) => (
+              <li key={items.id}>
+                <Link
+                  href={items.url}
+                  className="block py-2 pl-3 pr-4 font-normal text-gray-600 rounded hover:bg-gray-100 
+                  md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 
+                  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white 
+                  md:dark:hover:bg-transparent dark:border-gray-700"
+                  aria-current="page"
+                >
+                  {items.name}
+                </Link>
+              </li>
+            ))}
           </ul>
-        </div> */}
+        </div>
       </div>
     </nav>
   );
